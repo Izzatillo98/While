6d095ab4eb2294837ef1633ccd270531b7200043
Izzatillo Abdusamatov <abdusamatovtillo8@gmail.com>
2024-06-15 19:06:53 +0500

    2

--- Program.cs
+++ Program.cs
@@ -1,10 +1,31 @@
 ﻿using System;
-string name;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+string number1;
+int number = 1;
 do
-{
-    Console.WriteLine("Ism");
-    Console.WriteLine("Davom et agar (y)");
-    name = Console.ReadLine();
 
-}
-while(name == "y");
+
+{   do
+    
+    {
+     Console.WriteLine($" 1 x {number} = {number * 1} ");        
+     Console.WriteLine($" 2 x {number} = {number * 2} ");  
+     Console.WriteLine($" 3 x {number} = {number * 3} ");
+     Console.WriteLine($" 4 x {number} = {number * 4} ");
+     Console.WriteLine($" 5 x {number} = {number * 5} ");
+     Console.WriteLine($" 6 x {number} = {number * 6} ");
+     Console.WriteLine($" 7 x {number} = {number * 7} ");
+     Console.WriteLine($" 8 x {number} = {number * 8} ");
+     Console.WriteLine($" 9 x {number} = {number * 9} ");
+     Console.WriteLine("Do you want continue? (y)");
+     number1 = Console.ReadLine();
+     number ++;
+    }
+    while (number1=="y");
+
+    number++;
+}while(number<=9);
+
+
+
