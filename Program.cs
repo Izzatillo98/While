5a6ba71fb0f280456b356243e8909668c1cc1f0c
Izzatillo Abdusamatov <abdusamatovtillo8@gmail.com>
2024-06-15 19:13:06 +0500

    3

--- Program.cs
+++ Program.cs
@@ -9,20 +9,20 @@ do
 {   do
     
     {
-     Console.WriteLine($" 1 x {number} = {number * 1} ");        
-     Console.WriteLine($" 2 x {number} = {number * 2} ");  
-     Console.WriteLine($" 3 x {number} = {number * 3} ");
-     Console.WriteLine($" 4 x {number} = {number * 4} ");
-     Console.WriteLine($" 5 x {number} = {number * 5} ");
-     Console.WriteLine($" 6 x {number} = {number * 6} ");
-     Console.WriteLine($" 7 x {number} = {number * 7} ");
-     Console.WriteLine($" 8 x {number} = {number * 8} ");
-     Console.WriteLine($" 9 x {number} = {number * 9} ");
+     Console.WriteLine($" 1 x {number} = {number * 1} 2 x {number} = {number * 2} 3 x {number} = {number * 3} 4 x {number} = {number * 4} 5 x {number} = {number * 5} 6 x {number} = {number * 6} 7 x {number} = {number * 8} 8 x {number} = {number * 8} 9 x {number} = {number * 9} ");        
+    // Console.WriteLine($" 2 x {number} = {number * 2} ");  
+     //Console.WriteLine($" 3 x {number} = {number * 3} ");
+     //Console.WriteLine($" 4 x {number} = {number * 4} ");
+     //Console.WriteLine($" 5 x {number} = {number * 5} ");
+     //Console.WriteLine($" 6 x {number} = {number * 6} ");
+     //Console.WriteLine($" 7 x {number} = {number * 7} ");
+     //Console.WriteLine($" 8 x {number} = {number * 8} ");
+     //Console.WriteLine($" 9 x {number} = {number * 9} ");
      Console.WriteLine("Do you want continue? (y)");
      number1 = Console.ReadLine();
      number ++;
     }
-    while (number1=="y");
+    while (number<=9);
 
     number++;
 }while(number<=9);
