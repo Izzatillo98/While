d46ec4fe7f6d2db391316bffea9600cb74c72f8c
Izzatillo Abdusamatov <abdusamatovtillo8@gmail.com>
2024-06-15 21:11:29 +0500

    4

--- Program.cs
+++ Program.cs
@@ -2,31 +2,29 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 string number1;
-int number = 1;
+
 do
+        {        
+           Console.WriteLine("Karra jadvali");
+            int number;
+            int lastnumber;
+            Console.Write("Nechchi karradan : ");
+            string Inputnumber = Console.ReadLine();
+            number = Convert.ToInt32(Inputnumber);
+            Console.Write("Nechchi karragacha? : ");
+            string Inputlast = Console.ReadLine();
+            lastnumber = Convert.ToInt32(Inputlast);
+            while (number <= lastnumber)
+                {
 
-
-{   do
-    
-    {
-     Console.WriteLine($" 1 x {number} = {number * 1} 2 x {number} = {number * 2} 3 x {number} = {number * 3} 4 x {number} = {number * 4} 5 x {number} = {number * 5} 6 x {number} = {number * 6} 7 x {number} = {number * 8} 8 x {number} = {number * 8} 9 x {number} = {number * 9} ");        
-    // Console.WriteLine($" 2 x {number} = {number * 2} ");  
-     //Console.WriteLine($" 3 x {number} = {number * 3} ");
-     //Console.WriteLine($" 4 x {number} = {number * 4} ");
-     //Console.WriteLine($" 5 x {number} = {number * 5} ");
-     //Console.WriteLine($" 6 x {number} = {number * 6} ");
-     //Console.WriteLine($" 7 x {number} = {number * 7} ");
-     //Console.WriteLine($" 8 x {number} = {number * 8} ");
-     //Console.WriteLine($" 9 x {number} = {number * 9} ");
-     
-     number ++;
-    }
-    while (number<=9); 
-
-    number++;
-    Console.WriteLine("Do you want continue? (y)");
-     number1 = Console.ReadLine();
-}while(number<=9);
+                 Console.WriteLine($" 1 x {number} = {number * 1} 2 x {number} = {number * 2} 3 x {number} = {number * 3} 4 x {number} = {number * 4} 5 x {number} = {number * 5} 6 x {number} = {number * 6} 7 x {number} = {number * 8} 8 x {number} = {number * 8} 9 x {number} = {number * 9} ");        
+                 number ++;
+                }
+                        
+             Console.WriteLine("Do you want continue? (yes -> no)");
+             number1 = Console.ReadLine();
+        }
+        while (number1 == "yes");
 
 
 
